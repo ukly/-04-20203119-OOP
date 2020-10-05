a17315bfcfed13b2a692cfd663c0717ecd22873d
ukly <65989344+ukly@users.noreply.github.com>
2020-10-05 17:38:09 +0900

    add 20203119-Lab04 project folder

---
+++ Lab04/Lab04/src/no1/CompareTest.java
@@ -0,0 +1,15 @@
+package no1;
+import java.awt.*;
+
+class CompareTest {
+	public static void main(String[] args) {
+		int w = (int)888.8;
+		System.out.println(w);
+		byte x = (byte)100L;
+		System.out.println(x);
+		long y = (byte)100;
+		System.out.println(y);
+		byte z = (byte)1000L;
+		System.out.println(z);
+	}
+}
